c23786829fea4860f0cba8f5912b7f4a569e1329
Metin <23741460+mtndrms@users.noreply.github.com>
2024-03-31 10:57:40 +0300

    lazyvim

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = true,
+    priority = 1000,
+    opts = function()
+      return {
+        transparent = true,
+      }
+    end,
+  },
+}
